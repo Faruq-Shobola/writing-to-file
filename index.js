bf9664cd4b22bad20c124c48c40f790a7a66faa7
Shobola Faruq <shobolafaruq@gmail.com>
2021-04-19 23:44:53 +0100

    fetching and writing file to json

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const fetch = require('node-fetch');
 const fs = require('fs');
 
+fetch('http://jsonplaceholder.typicode.com/posts')
+.then(res => res.json())
+.then(json => {
+    let data = JSON.stringify(json, null, 2);
+
+    fs.writeFile('result/posts.json', data, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+});
+})
 const server = http.createServer(function(req, res) {
 
-  
+
     res.writeHead(200);
     res.end('Hello world');
 
